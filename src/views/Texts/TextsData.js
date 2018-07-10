c9380b2828f7a28ef92bd5faed758ff2bd5fd37e
asautour <amaury.sautour@gmail.com>
2018-07-10 07:53:00 +0100

    cleanup

--- src/views/Texts/TextsData.js
+++ src/views/Texts/TextsData.js
@@ -3,7 +3,7 @@ const txtData = [
     _id: 'MIFIDII',
     name: 'MiFID II',
     level: '1',
-    regBody: 'ESMA',
+    regBody: 'France AMF',
     published: '31/12/2015',
     status: 'Draft',
     link: 'https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:32014L0065',
@@ -11,6 +11,7 @@ const txtData = [
   {
     _id: 'MIFIR',
     name: 'MiFIR',
+    level: '1',
     regBody: 'ESMA',
     published: '31/01/2016',
     status: 'Published',
@@ -19,6 +20,7 @@ const txtData = [
   {
     _id: 'DR250416',
     name: 'Delegated Regulation of 25/04/16',
+    level: '2',
     regBody: 'ESMA',
     published: '25/04/2016',
     status: 'Final',
@@ -27,6 +29,7 @@ const txtData = [
   {
     _id: 'DD170616',
     name: 'Delegated Directive of 17/06/16',
+    level: '2',
     regBody: 'ESMA',
     published: '17/06/2016',
     status: 'Final draft',
--- src/views/Texts/TextsListItem.js
+++ src/views/Texts/TextsListItem.js
@@ -20,7 +20,6 @@ export default class TextListIteam extends Component {
     this.setState({ edit: !this.state.edit });
   }
 
-
   render() {
     const { text } = this.props;
 
@@ -33,13 +32,19 @@ export default class TextListIteam extends Component {
             <Col xs="6">
               <FormGroup row>
                 <Col md="4"><Label htmlFor="text-input">{LABEL_NAME}</Label></Col>
-                <Col xs="12" md="8"><Input type="text" id="text-name" name="text-name" value={text.name} /></Col>
+                <Col xs="12" md="8">
+                  {this.state.edit && <Input type="text" id="text-name" name="text-name" value={text.name} />}
+                  {!this.state.edit && <Label>{text.name}</Label> }
+                </Col>
               </FormGroup>
             </Col>
             <Col xs="6">
               <FormGroup row>
-                <Col md="4"><Label htmlFor="date-input">Published on</Label></Col>
-                <Col xs="12" md="8"><Input type="text" id="text-level" name="text-level" value={text.level} /></Col>
+                <Col md="4"><Label htmlFor="text-input">Level</Label></Col>
+                <Col xs="12" md="8">
+                  {this.state.edit && <Input type="text" id="text-level" name="text-level" value={text.level} />}
+                  {!this.state.edit && <Label>{text.level}</Label> }
+                </Col>
               </FormGroup>
             </Col>
           </Row>
@@ -48,9 +53,10 @@ export default class TextListIteam extends Component {
           <FormGroup row>
             <Col md="2"><Label htmlFor="select">{LABEL_REGULATORY_BODY}</Label></Col>
             <Col md="10">
-              <Input type="select" name="select" id="select" >
+              {this.state.edit && <Input type="select" name="select" id="select" >
                 { regBodies.map((body, index) => <option value={index}>{body}</option>)}
-              </Input>
+              </Input>}
+              {!this.state.edit && <Label>{text.regBody}</Label>}
             </Col>
           </FormGroup>
 
@@ -59,13 +65,19 @@ export default class TextListIteam extends Component {
             <Col xs="6">
               <FormGroup row>
                 <Col md="4"><Label htmlFor="date-input">Published on</Label></Col>
-                <Col xs="12" md="8"><Input type="date" id="date-published" name="date-published" value={text.published} /></Col>
+                <Col xs="12" md="8">
+                  {this.state.edit && <Input type="date" id="date-published" name="date-published" value={text.published} />}
+                  {!this.state.edit && <Label>{text.published}</Label>}
+                </Col>
               </FormGroup>
             </Col>
             <Col xs="6">
               <FormGroup row>
                 <Col md="4"><Label htmlFor="text-input">Status</Label></Col>
-                <Col xs="12" md="8"><Input type="text" id="text-status" name="text-status" value={text.status} /></Col>
+                <Col xs="12" md="8">
+                  {this.state.edit && <Input type="text" id="text-status" name="text-status" value={text.status} />}
+                  {!this.state.edit && <Label>{text.status}</Label>}
+                </Col>
               </FormGroup>
             </Col>
           </Row>
@@ -73,7 +85,9 @@ export default class TextListIteam extends Component {
           {/* HTTP link to the original text */}
           <FormGroup row>
             <Col md="2"><Label htmlFor="select">{LABEL_LINK_TO_ORIGINAL}</Label></Col>
-            <Col xs="10" md="8"><a href={`${text.link}`} target="_blank">{text.link}</a>
+            <Col xs="10" md="8">
+              {!this.state.edit && <a href={`${text.link}`} target="_blank">{text.link}</a>}
+              {this.state.edit && <Input type="text" id="text-status" name="text-status" value={text.link} />}
             </Col>
           </FormGroup>
 
@@ -84,7 +98,8 @@ export default class TextListIteam extends Component {
           </FormGroup>
         </Form>
 
-        <Button type="submit" className="btn btn-outline-primary" nClick={this.toggleEdit}>Edit</Button>
+        {this.state.edit && <Button type="submit" className="btn btn-outline-primary" onClick={this.toggleEdit}>Save</Button>}
+        {!this.state.edit && <Button type="submit" className="btn btn-outline-primary" onClick={this.toggleEdit}>Edit</Button>}
 
       </div>
     );
